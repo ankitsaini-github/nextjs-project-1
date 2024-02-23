22f6686d52a8cbe2f3ecedc253e17f63e25410f4
ankitsaini-github <ankit121saini@gmail.com>
2024-02-23 11:52:13 +0530

    Link tag used to link pages

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function AboutusPage(){
-  return <h1>aboutus</h1>
+  return <>
+    <h1>aboutus</h1>
+    <ul>
+      <li><Link href={'/aboutus/1'}>Yash</Link></li>
+      <li><Link href={'/aboutus/2'}>Vaibhav</Link></li>
+      <li><Link href={'/aboutus/3'}>Suresh</Link></li>
+    </ul>
+  </>
 }
